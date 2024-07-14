7d0dcd6aeda10526c55e57c5730d4b818f5234ba
jinwon <choiok1231@gmail.com>
2024-07-14 09:22:33 +0900

    programmers

---
+++ Lv0/간단한 식 계산하기.py
@@ -0,0 +1,15 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+
+    a = int(a)
+    b = int(b)
+
+    if op == "+":
+        result = a + b
+    elif op == "-":
+        result = a - b
+    elif op == "*":
+        result = a * b
+
+    return result
+
