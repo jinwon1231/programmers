01309e6f040b651e255074eb4bdd70b8b214eccb
jinwon <choiok1231@gmail.com>
2024-08-30 12:33:42 +0900

    programmers

---
+++ Lv1/추억 점수.py
@@ -0,0 +1,9 @@
+def solution(name, yearning, photo):
+    answer = []
+    for i in photo:
+        score = 0
+        for j in i:
+            if j in name:
+                score = score + yearning[name.index(j)]
+        answer.append(score)
+    return answer
